a51b3555b4f6f008302ff876a86939d00c2577e3
Dmither <mr.d.graumann@gmail.com>
2023-03-02 12:06:21 +0200

    add footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import * as functions from "./modules/functions.js"
 import { burger } from "./modules/menu-burger.js";
+import {InitSpoiler} from "./modules/onlyOneSpoiler.js";
 
 
 let burgerBtn = document.querySelector(".header__burger");
@@ -7,3 +8,9 @@ let menu = document.querySelector(".header__buttons")
 
 burger(burgerBtn, menu);
 
+
+let accordions = document.querySelectorAll(".category");
+console.log(document.documentElement.offsetWidth)
+if (document.documentElement.offsetWidth < 768){
+  InitSpoiler(accordions);
+}
